6ba4f76ca198d495b3cb0adf1070c1c9c5e4b114
Serena-Dong <serenadong3159@icloud.com>
2023-03-15 12:01:55 +0100

    index btns

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -24,16 +24,26 @@
                         <th scope="col">Visible</th>
                         <th scope="col">Created</th>
                         <th scope="col">Updated</th>
+                        <th scope="col">Actions</th>
                       </tr>
                     </thead>
                     <tbody>
                         @foreach ($projects as $project)
                       <tr>
-                        <th scope="row"><a style="text-transform: uppercase;" href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></th>
-                        <td><input type="text" disabled value='{{Str::slug(old('title', $project->title), '-')}}'></td>
-                        <td>{{$project->is_published ? 'Yes' : 'No'}}</td>
-                        <td>{{$project->created_at}}</td>
-                        <td>{{$project->updated_at}}</td>
+                        <th class="align-middle" scope="row"><a style="text-transform: uppercase;" href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></th>
+                        <td class="align-middle"><input type="text" disabled value='{{Str::slug(old('title', $project->title), '-')}}'></td>
+                        <td class="align-middle">{{$project->is_published ? 'Yes' : 'No'}}</td>
+                        <td class="align-middle">{{$project->created_at}}</td>
+                        <td class="align-middle">{{$project->updated_at}}</td>
+                        <td class="align-middle d-flex">
+                          <a class="btn btn-primary" href="{{route('admin.projects.show', $project->id)}}"><i class="fa-solid fa-eye text-white"></i></a>
+                          <a class="btn btn-warning mx-1" href="{{route('admin.projects.edit', $project->id)}}"><i class="fa-solid fa-pen-to-square text-white"></i></a>
+                          <form class="delete-form" action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" data-project-name="{{ $project->name }}">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-small btn-danger me-2"><i class="fa-solid fa-trash"></i></button>
+                        </form>
+                        </td>
                       </tr>
                       @endforeach
 
