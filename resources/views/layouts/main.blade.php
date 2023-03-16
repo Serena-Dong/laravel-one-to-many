7f4ba3215f9ca419484ad6e72f2f44178c89ab72
Serena-Dong <serenadong3159@icloud.com>
2023-03-16 09:49:35 +0100

    changing the name from 'app' to 'main'

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ env('APP_NAME') }} | @yield('title')</title>
 
 
     <!-- Fonts -->
